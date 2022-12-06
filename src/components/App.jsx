388f81a9887b599fc41a2c2d8dceb8dfde263140
Alina Romanova <alinakhraban@gmail.com>
2022-12-06 20:02:13 +0200

    add edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,16 @@ import ContactForm from './ContactForm';
 export class App extends Component {
 
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+    name: '',
+    number: ''
   }
-
   addContact = data => {
     console.log(data)
     const newContact = {
@@ -24,13 +30,22 @@ export class App extends Component {
 
 
   render() {
-
+const {contacts} = this.state
     return (
       <>
 
-      <ContactForm  handleNameChange = {this.handleNameChange} addUser={this.addUser} />
+      <ContactForm  handleNameChange = {this.handleNameChange} onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <ul>
+       {contacts.map(contact => (
+        <li key={contact.id} >
+          <p
 
-      <h2>Contacts</h2>
+          > {contact.name}</p>
+        </li>
+      ))}
+       </ul>
+
       {/* <Filter />
       <ContactList />  */}
     </>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { nanoid } from 'nanoid';
 
 class ContactForm extends Component {
 
@@ -7,6 +8,8 @@ class ContactForm extends Component {
     name: ''
   };
 
+  loginInputId = nanoid();
+
   handleNameChange = (event) => {
     this.setState({
       name: event.target.value,
@@ -14,10 +17,14 @@ class ContactForm extends Component {
   }
 
   handlerSubmit = (e) => {
-    e.preventDefault()
-     this.props.addContact(this.state)
 
-     this.setState = {
+    e.preventDefault()
+     this.props.onSubmit(this.state)
+     this.reset();
+  }
+
+  reset = () => {
+    this.setState = {
       contacts: [],
       name: '',
     }
@@ -25,19 +32,22 @@ class ContactForm extends Component {
 
 
   render() {
-    const {name} = this.state;
+    const {name, loginInputId} = this.state;
   return (
     <form onSubmit={this.handlerSubmit}>
       <h1>Phonebook</h1>
+      <label htmlFor=""> Name
       <input
   type="text"
   name="name"
   value={name}
+  id = {loginInputId}
   pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
   required
   onChange={this.handleNameChange}
 />
+</label>
 <button>Add contacts</button>
     </form>
   )
